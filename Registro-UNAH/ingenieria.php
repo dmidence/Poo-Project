1a3d99c6bf92b827395475817c2ddd4b359e11ff
dmidence <40371373+dmidence@users.noreply.github.com>
2018-12-11 01:18:41 -0600

    Create ingenieria.php

---
+++ Registro-UNAH/ingenieria.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Ingenieria</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="img/ingenieria.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+
+
+
+
+<style>
+body{
+	background-image: url("img/fondogris.jpeg");
+}
+
+
+</style>
+
+</head>
+
+<body>
+	<img class="logo" src="img/Logo-UNAH.png" alt="Third slide">
+
+
+
+	<?php
+include ("navfacu.html") ;
+  ?> 
+	
+	
+
+
+	
+
+
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+					<div class="table">
+
+						<div class="row header">
+							<div class="cell">
+								Carrera
+							</div>
+							<div class="cell">
+								Indice PAA
+							</div>
+							<div class="cell">
+								Centros
+							</div>
+							<div class="cell">
+								Plan de Estudio
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+									INGENIERIA CIVIL  
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								1000
+							</div>
+							<div class="cell" data-title="centros">
+								CU,UNAHVS
+							</div>
+							<div class="cell"  data-title="Plan de Estudio">
+								<a title="plan de ingenieria"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Ingenieria%20Civil%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+								INGENIERIA MECANICA INDUSTRIAL   
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								1000
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="plan de ingenieria"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/INGENIER%C3%8DA%20MEC%C3%81NICA%20INDUSTRIAL%202018.pdf">
+									<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+									INGENIERIA QUIMICA INDUSTRIAL    
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								1000
+							</div>
+							<div class="cell" data-title="centros">
+									CU
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="plan de ingenieria"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/INGENIER%C3%8DA%20QU%C3%8DMICA%20INDUSTRIAL%202018.pdf">
+									<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+									INGENIERIA ELECTRICA INDUSTRIAL    
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								1000
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="plan de ingenieria"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Ingenieria%20Electrica%202018.pdf">
+									<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+									INGENIERIA INDUSTRIAL   
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								1000
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="plan de ingenieria"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Ingenieria%20Industrial%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+									INGENIERIA EN SISTEMAS  
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								1000
+							</div>
+							<div class="cell" data-title="centros">
+									CU, UNAHVS, CUROC, CURLP
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="plan de ingenieria"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/INGENIER%C3%8DA%20EN%20SISTEMAS%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						
+
+					</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
