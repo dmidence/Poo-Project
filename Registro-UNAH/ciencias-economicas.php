e8255fec9039b190d9475445d10d59b41e54b48c
dmidence <40371373+dmidence@users.noreply.github.com>
2018-12-11 01:17:13 -0600

    Create ciencias-economicas.php

---
+++ Registro-UNAH/ciencias-economicas.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Ciencias Economicas</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="img/economicas.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+
+
+
+
+<style>
+body{
+	background-image: url("img/fondogris.jpeg");
+}
+
+
+</style>
+
+</head>
+
+<body>
+	<img class="logo" src="img/Logo-UNAH.png" alt="Third slide">
+
+
+
+	<?php
+include ("navfacu.html") ;
+  ?> 
+	
+	
+
+
+	
+
+
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+					<div class="table">
+
+						<div class="row header">
+							<div class="cell">
+								Carrera
+							</div>
+							<div class="cell">
+								Indice PAA
+							</div>
+							<div class="cell">
+								Centros
+							</div>
+							<div class="cell">
+								Plan de Estudio
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN ADMINISTRACION DE EMPRESAS  
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								900
+							</div>
+							<div class="cell" data-title="centros">
+								CU,UNAHVS,CURLA,CURC,CUROC, CURLP
+							</div>
+							<div class="cell"  data-title="Plan de Estudio">
+								<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Administraci%C3%B3n%20de%20Empresas%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+								LICENCIATURA EN ADMINISTRACION PUBLICA        
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Administraci%C3%B3n%20Publica%202018.pdf">
+									<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN ECONOMIA         
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Economia%202018.pdf">
+									<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN CONTADURIA PUBLICA Y FINANZAS
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Contaduria%20Publica%20y%20Finanzas%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN ADMINISTRACION ADUANERA     
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Administraci%C3%B3n%20Aduanera%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+                        </div>
+                        
+                        <div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN MERCADOTECNIA
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU,UNAHVS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/MERCADOT%C3%89CNIA%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+                        </div>
+                        
+                        <div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN ADMINISTRACION DE BANCA Y FINANZAS    
+                            							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/BANCA%20Y%20FINANZAS%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+                        </div>
+                        
+                        <div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENTURA EN COMERCIO INTERNACIONAL 
+                            							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+									CU
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/COMERCIO%20INTERNACIONA%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+                        </div>
+                        
+                        <div class="row">
+							<div class="cell" data-title="carrera">
+                                LICENCIATURA EN INFORMATICA ADMINISTRATIVA  
+							</div>
+							<div class="cell" data-title="indice-requerido">
+								700
+							</div>
+							<div class="cell" data-title="centros">
+                                CU, UNAH-TD,UNAH-VS
+							</div>
+							<div class="cell" data-title="Plan de Estudio">
+									<a title="planes"  target="_blank" href="http://www.admisiones.unah.edu.hn/Portals/0/planes%202018/Informatica%20Administrativa%202018.pdf">
+								<img src="img/pdf.png" alt="pdf" ></a>
+							</div>
+						</div>
+
+						
+
+					</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
