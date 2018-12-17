bb052e9d5d8e7da7508d7c92b60f7ebec6d10af5
dmidence <40371373+dmidence@users.noreply.github.com>
2018-12-17 11:50:05 -0600

    Update adiclas.php

--- Registro-UNAH/adiclas.php
+++ Registro-UNAH/adiclas.php
@@ -11,17 +11,52 @@
     <title>Matricula</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="img/logo.ico">
-    <link href="css/matricula.css" rel="stylesheet"> 
-    <link href="signinestudiante/css/fa-svg-with-js.css" rel="stylesheet"> 
+    <script src="js/jquery.min.js"></script> 
+    <link href="signinestudiante/css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="signinestudiante/css/matri.css" rel="stylesheet"> 
+    <link href="signinestudiante/css/fa-svg-with-js.css" rel="stylesheet">
+     
 </head>
 <body>
 <?php
 include ("header.html") ;
 ?>
+<div class="container" style="margin-top:100px">
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
 
 
 
 
-
+<script src="js/bootstrap.min.js"></script> 
+<script src="signinestudiante/js/fontawesome-all.min.js"></script> 
+ <script src="signinestudiante/js/controlador.js"></script>
 </body>
 </html>
