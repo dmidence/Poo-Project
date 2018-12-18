fc1dc72c2497571da2a0b1e06e854c09b3e18cd7
dmidence <40371373+dmidence@users.noreply.github.com>
2018-12-18 12:02:51 -0600

    Create controlador-matricula.js

---
+++ Registro-UNAH/js/controlador-matricula.js
@@ -0,0 +1,183 @@
+//obtener las facultades//
+$(document).ready(function(){
+	console.log("Se ha cargado el DOM");
+	$.ajax({
+		url:"ajax/facultad.php",
+		method:"GET",
+		dataType:"json",
+		success:function(respuesta){
+            console.log(respuesta);
+			$("#slc-facultades").append(`<option value="seleccionar">Seleccione la faculdad</option>`);
+			for (var i =0;i<respuesta.length;i++){
+                $("#slc-facultades").append(`
+                <option value="${respuesta[i].abreviatura}">${respuesta[i].Nombre}</option>`
+				);
+			}
+		},
+		error:function(error){
+			console.error(error);
+		}
+	});
+
+////////////////////////////////////////////VISUALIZAR MATRICULA //////////////////////////////////////////////////////
+	var parametro  = "cuenta="+$("#txt-cuenta").val()+"&"
+					+"carrera="+$("#txt-carrera").val()+"&"
+					+"facultad="+$("#txt-facultad").val();
+		console.log(parametro);
+        $.ajax({
+		url:"ajax/matricula.php?accion=2", //opcion 2 para obtener asignaturas matriculadas
+		method:"GET",
+		data:parametro,
+		dataType:"json",
+		
+		success:function(respuesta){
+			console.log(respuesta);
+
+				for (var i =0;i<respuesta.length;i++){
+					$("#clases-matriculadas").append(
+						`<tr>
+							<td>${respuesta[i].codCarrera}${respuesta[i].codClase}</td>
+							<td>${respuesta[i].seccion}</td>
+							<td>${respuesta[i].clase}</td>
+							<td>${respuesta[i].inicio}</td>
+							<td>${respuesta[i].final}</td>
+							<td>${respuesta[i].dias}</td>
+							<td>${respuesta[i].uv}</td>
+							<td>3</td>
+						</tr>`
+					);
+				}
+				
+			}	
+		
+	});
+
+
+
+
+});
+
+//====================================================obtener carreras=================================================================
+$("#slc-facultades").change(function(){
+	var facultad = $("#slc-facultades").val();
+	
+	//Esta funcion se ejecuta cada vez que el usuario selecciona o cambia un elemento de la lista.	
+	$.ajax({
+		url:"ajax/carreras.php?accion=1",			//la accion 1 es para obtener las carreras
+		method:"GET",
+		data: 'facultad='+facultad,
+		dataType:"json",
+		success:function(respuesta){
+            console.log(respuesta);
+            $("#select-carreras").append(`<option value="seleccionar">Seleccione la carrera</option>`);
+			for (var i =0;i<respuesta.length;i++){
+                $("#select-carreras").append(`
+                <option value="${respuesta[i].codigo}">${respuesta[i].carrera}</option>`
+				);
+			}
+		},
+		error:function(error){
+			console.error(error);
+			$("#jefe").append(error.responseText);
+		}
+	});
+});
+
+//====================================================obtener clases=================================================================
+$("#select-carreras").change(function(){
+	var carrera = $("#select-carreras").val();
+	var facultad = $("#slc-facultades").val();
+	//Esta funcion se ejecuta cada vez que el usuario selecciona o cambia un elemento de la lista.	
+	$.ajax({
+		url:"ajax/clases.php?accion=1",			//la accion 1 es para obtener las carreras
+		method:"GET",
+        data: "carrera="+carrera+"&"
+              +"facultad="+facultad,
+		dataType:"json",
+		success:function(respuesta){
+            console.log(respuesta);
+        
+			for (var i =0;i<respuesta.length;i++){
+                $("#select-clases").append(`
+                <option value="${respuesta[i].codigo}">${respuesta[i].clase}</option>`
+				);
+			}
+		},
+		error:function(error){
+			console.error(error);
+			$("#jefe").append(error.responseText);
+		}
+	});
+});
+
+//====================================================obtener secciones=================================================================
+$("#select-clases").change(function(){
+	var carrera = $("#select-carreras").val();
+    var facultad = $("#slc-facultades").val();
+    var clase = $("#select-clases").val();
+
+	//Esta funcion se ejecuta cada vez que el usuario selecciona o cambia un elemento de la lista.	
+	$.ajax({
+		url:"ajax/secciones.php?accion=1",			//la accion 1 es para obtener las seccioness
+		method:"GET",
+        data: "codCarrera="+carrera+"&"
+              +"facultad="+facultad+"&"
+              +"codClase="+clase,
+		dataType:"json",
+		success:function(respuesta){
+            console.log(respuesta);
+
+			for (var i =0;i<respuesta.length;i++){
+                $("#select-secciones").append(`
+                <option value="${respuesta[i].seccion}"><b>Seccion:</b> ${respuesta[i].seccion} <b>cupos:</b> ${respuesta[i].cupos}   <b>dias:</b> ${respuesta[i].dias}   <b>Docente:</b> ${respuesta[i].docente}</option>`
+				);
+			}
+		},
+		error:function(error){
+			console.error(error);
+			$("#jefe").append(error.responseText);
+		}
+	});
+});
+
+////////////////////////////////////////////MATRICULAR ASIGNATURA//////////////////////////////////////////////////////
+
+$("#btn-matricular").click(function(){
+	var parametros = "seccion="+$("#select-secciones").val()+"&"+
+					"codCarrera="+$("#select-carreras").val()+"&"+
+					"cuenta="+$("#usuario-matriculando").val()+"&"+
+					"codClase="+$("#select-clases").val()+"&"+
+					"facultad="+$("#slc-facultades").val();
+			
+			console.log(parametros);		
+			$("#btn-matricular").attr("disabled",true);
+			
+			$.ajax({
+				url:"ajax/matricula.php?accion=1",  //accion 1 para matricular clases
+				data:parametros,
+				method:"POST",
+				dataType:"json",
+				success:function(respuesta){
+					console.log(respuesta);
+				//	$('#modal-matricular').modal('hide');		
+						$("#clases-matriculadas").append(
+							`<tr>
+								<td>${respuesta.codCarrera}${respuesta.codClase}</td>
+								<td>${respuesta.seccion}</td>
+								<td>${respuesta.clase}</td>
+								<td>${respuesta.inicio}</td>
+								<td>${respuesta.final}</td>
+								<td>${respuesta.dias}</td>
+								<td>${respuesta.uv}</td>
+								<td>3</td>
+							</tr>`
+						);
+					}	
+				
+			});
+});
+
+
+
+
+
