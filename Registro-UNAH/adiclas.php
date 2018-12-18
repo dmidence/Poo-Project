e3e788dd558ec66ff5c01b2752c5d3ec5b078b33
dmidence <40371373+dmidence@users.noreply.github.com>
2018-12-18 12:02:27 -0600

    Update adiclas.php

--- Registro-UNAH/adiclas.php
+++ Registro-UNAH/adiclas.php
@@ -1,7 +1,7 @@
 <?php
     session_start();  
     if (!isset($_SESSION["NoCuenta"]))
-        header("Location: login-estudiantes.php");//Redireccion con PHP
+        header("Location: login-estudiantes.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,42 +21,58 @@
 <?php
 include ("header.html") ;
 ?>
-<div class="container" style="margin-top:100px">
+<div class="container" style="margin-top:150px">
+          <button id="btnadic" style=""  class="btn btnad" type="button" data-toggle="modal" data-target="#adicionar">Adicionar Asignatura</button>
+                    </div>
+ 
+                        
+</div>
+<div class="modal fade" id="adicionar" role="dialog">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            
+              <!-- Modal content-->
+            <div class="modal-content" style="background-color:rgb(1, 49, 95);">
+                <div class="modal-header centrar" >
+                  <h6 class="modal-title" style="color:white"><b>Asignaturas</b></h6>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body" style="background-color:rgb(255, 255, 255);" class="col-12 col-sm-12 col-md-6 col-lg-6 form-control">                
+                     <div class="row centrar">               
+                         <div class="col-12" style="margin-bottom:2%">
+  
+                            <div>
+                                    <select name="facultades" id="facu" class="form-control col-12 col-sm-12 col-md-12 col-lg-12 " style="float:left">
+                                        
+                                    </select>
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+                                   
+                                    <select multiple class="form-control col-12 col-sm-12 col-md-12 col-lg-12 " id="clases"  size="7">
+                                    <input type="text" id="txt-carpeta-actual" style="display:none" class="form-control" value="<?php echo isset($_GET['carpeta'])?$_GET['carpeta']:''; ?>" disabled>                                  
+                                    </select>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+                                    <select multiple class="form-control col-12 col-sm-12 col-md-12 col-lg-12 " id="select-secciones" size="7">
+                                            
+                                    </select>
+                            </div>
+
+                         </div>  
+                         <br>                                                    
+                     </div>
+                </div>  
+                <div class="modal-footer">
+                    <button class="btn btnad centrar"  type="button" value="matricular" id="btn-matricular">Matricular</button>
+                </div>                              
+            </div>
+        </div>
+          
     </div>
-  </div>
-</div>
 
 
-</div>
+ <script src="js/jquery-3.3.1.js"></script>     
+    <script src="js/bootstrap.min.js"></script>
+    <script src="signinestudiante/js/controlador.js"></script>
 
-
-
-
-<script src="js/bootstrap.min.js"></script> 
 <script src="signinestudiante/js/fontawesome-all.min.js"></script> 
- <script src="signinestudiante/js/controlador.js"></script>
+
 </body>
 </html>
