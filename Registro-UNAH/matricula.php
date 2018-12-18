fcac85048f4a142f35fcab7fdc16de703ccb6734
dmidence <40371373+dmidence@users.noreply.github.com>
2018-12-18 12:47:40 -0600

    Update matricula.php

--- Registro-UNAH/matricula.php
+++ Registro-UNAH/matricula.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Matricula</title>
+    <title>Estudiante</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="img/logo.ico">
     <link href="css/matricula.css" rel="stylesheet"> 
